52e2e64aa460a370d565b9be8a08d4247158bf55
Linda <lindachch11@gmail.com>
2025-05-16 18:06:32 +0200

    Initial commit: Skapade en enkel kalkylator

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EnkelKalkylator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enkel Kalkylator");
+            Console.WriteLine("Ange Operation (+, -, *, /):");
+            char operation = Console.ReadKey().KeyChar;
+            Console.WriteLine("Ange första talet:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ange andra talet:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+
+                default:
+                    Console.WriteLine("Ogiltig Operation");
+                    return;
+            }
+            Console.WriteLine($"Resultat: {result}");
+            Console.ReadKey();
+        }
+    }
+}
+
